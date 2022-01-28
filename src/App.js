89a5e562d86dac4e2771dc79264683759e59f0f6
Mahmoud <mahmoudabdulmuty@gmail.com>
2022-01-28 16:18:56 +0200

    added fetch data to App Component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,33 @@
+import React from 'react';
 import './App.css';
+import Article from './components/Article';
 
-function App() {
-	return <div className="App"></div>;
+class App extends React.Component {
+	state = {
+		articles: []
+	};
+	componentDidMount() {
+		fetch(
+			'http://newsapi.org/v2/top-headlines?country=eg&apiKey=3bd4753c68144c04b3eb73e44b7da657'
+		)
+			.then((res) => res.json())
+			.then((data) =>
+				this.setState({
+					articles: data.articles
+				})
+			);
+	}
+	render() {
+		return (
+			<>
+				<header className="header">
+					<h1 className="header-title">أخبار مصر</h1>
+				</header>
+				<div className="app">
+					<Article articles={this.state.articles} />
+				</div>
+			</>
+		);
+	}
 }
-
 export default App;
