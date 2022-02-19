0354da70e04826cdb9331a7ab7d6926b31eea27e
Mahmoud <mahmoudabdulmuty@gmail.com>
2022-02-19 20:13:46 +0200

    convert class components to hooks

--- src/App.js
+++ src/App.js
@@ -1,33 +1,29 @@
-import React from 'react';
+import 'noto-sans-arabic/css/300.css';
+import 'noto-sans-arabic/css/400.css';
+import 'noto-sans-arabic/css/700.css';
+import { useEffect, useState } from 'react';
 import './App.css';
 import Article from './components/Article';
 
-class App extends React.Component {
-	state = {
-		articles: []
-	};
-	componentDidMount() {
+function App() {
+	const [articles, setArticles] = useState([]);
+	useEffect(() => {
 		fetch(
 			'http://newsapi.org/v2/top-headlines?country=eg&apiKey=3bd4753c68144c04b3eb73e44b7da657'
 		)
 			.then((res) => res.json())
-			.then((data) =>
-				this.setState({
-					articles: data.articles
-				})
-			);
-	}
-	render() {
-		return (
-			<>
-				<header className="header">
-					<h1 className="header-title">أخبار مصر</h1>
-				</header>
-				<div className="app">
-					<Article articles={this.state.articles} />
-				</div>
-			</>
-		);
-	}
+			.then((data) => setArticles(data.articles));
+	}, []);
+
+	return (
+		<>
+			<header className="header">
+				<h1 className="header-title">أخبار مصر</h1>
+			</header>
+			<div className="app">
+				<Article articles={articles} />
+			</div>
+		</>
+	);
 }
 export default App;
